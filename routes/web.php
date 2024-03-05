d04715b8dabeb710f45b7bd76bc02778dc710cc7
thasan <thasan20152016@gmail.com>
2024-03-05 19:39:00 +0600

    all

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Frontend\HomepageController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomepageController::class, 'homepage'])->name('homepage');
+
+
+Auth::routes();
+
+Route::get('/dashboard', [DashboardController::class,'dashboard'])->name('dashboard');
